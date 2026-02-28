3c3731bf9115b4deb1e09613c2e05a4f540ab034
Badreddine Barhoumi <barhoumi.badreddine@gmail.com>
2026-02-28 07:17:46 -0500

    java 25

--- src/main/java/com/logmein/interview/badreddinesDemo/BadreddinesDemoApplication.java
+++ src/main/java/com/logmein/interview/badreddinesDemo/BadreddinesDemoApplication.java
@@ -1,12 +1,17 @@
 package com.logmein.interview.badreddinesDemo;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BadreddinesDemoApplication {
 
 	public static void main(String[] args) {
+		log.atInfo().setMessage("Hello World!").addKeyValue("name", "logmein").addKeyValue("key", "value").log() ;
+
 		SpringApplication.run(BadreddinesDemoApplication.class, args);
 	}
 }
--- src/main/java/com/logmein/interview/badreddinesDemo/services/CardDeckServiceImpl.java
+++ src/main/java/com/logmein/interview/badreddinesDemo/services/CardDeckServiceImpl.java
@@ -1,19 +1,5 @@
 package com.logmein.interview.badreddinesDemo.services;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-
 import com.logmein.interview.badreddinesDemo.constants.AppResponses;
 import com.logmein.interview.badreddinesDemo.dao.GameDeckCardRepo;
 import com.logmein.interview.badreddinesDemo.dao.GameRepo;
@@ -27,160 +13,160 @@ import com.logmein.interview.badreddinesDemo.services.beans.card.CardCountCompar
 import com.logmein.interview.badreddinesDemo.services.beans.card.CardSuit;
 import com.logmein.interview.badreddinesDemo.services.beans.deck.DeckOfCards;
 import com.logmein.interview.badreddinesDemo.utilities.CardUtils;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
+@AllArgsConstructor
 @Service(value = "cardDeckService")
 public class CardDeckServiceImpl implements CardDeckService {
-	@Autowired
-	@Qualifier("deckCardCreatorService")
-	private DeckCardCreatorService deckCardCreatorService;
 
-	@Autowired
-	private GameRepo gameRepo;
-	@Autowired
-	private GameDeckCardRepo gameDeckCardRepo;
+    public static final String SAME_GAME_NAME_MESSAGE = "More than one game have the same 'gameName'!";
+    private DeckCardCreatorService deckCardCreatorService;
+    private GameRepo gameRepo;
+    private GameDeckCardRepo gameDeckCardRepo;
 
-	@Override
-	public Game addDeckToGameDeck(String gameName) {
+    @Override
+    public Game addDeckToGameDeck(@NonNull String gameName) {
 
-		final List<Game> lstGames = this.gameRepo.findByName(gameName);
-		if (lstGames.size() > 1) {
-			throw new AppException(AppResponses.ENTITY_ALREADY_EXIST, "More than one game have the same 'gameName'!");
-		} else if (lstGames.size() == 1) {
-			final Game game = lstGames.get(0);
-			final List<GameDeckCard> lstGameDeckCard = createGameDeckCardEntities(game);
-			this.addTheDeckGameCard(game, lstGameDeckCard);
-			return game;
-		} else {
-			throw new AppException(AppResponses.ENTITY_NOT_FOUND, "No Game with name[%s]!".formatted(gameName));
-		}
+        final List<Game> lstGames = this.gameRepo.findByName(gameName);
+        if (lstGames.size() > 1) {
+            throw new AppException(AppResponses.ENTITY_ALREADY_EXIST, SAME_GAME_NAME_MESSAGE);
+        } else if (lstGames.size() == 1) {
+            final Game game = lstGames.get(0);
+            final List<GameDeckCard> lstGameDeckCard = createGameDeckCardEntities(game);
+            this.addTheDeckGameCard(game, lstGameDeckCard);
+            return game;
+        } else {
+            throw new AppException(AppResponses.ENTITY_NOT_FOUND, "No Game with name[%s]!".formatted(gameName));
+        }
 
-	}
+    }
 
-	/**
-	 * create a list of 'GameDeckCard' entities.
-	 * 
-	 * @param game
-	 * @return
-	 */
-	List<GameDeckCard> createGameDeckCardEntities(final Game game) {
-		Assert.notNull(game, "[Assertion failed] - 'game' argument is required; it must not be null");
-		final DeckOfCards deckOfCards = this.deckCardCreatorService.createDeckOfCards();
-		final List<Card> cards = deckOfCards.getCards();
-		final List<GameDeckCard> lstGameDeckCard = new ArrayList<GameDeckCard>();
-		cards.forEach(card -> {
-			final GameDeckCard gameDeckCard = createGameDeckCard(game, card);
-			lstGameDeckCard.add(gameDeckCard);
-		});
-		updateGameDeckCardOrder(lstGameDeckCard);
-		return lstGameDeckCard;
-	}
+    /**
+     * create a list of 'GameDeckCard' entities.
+     *
+     * @param game
+     * @return
+     */
+    List<GameDeckCard> createGameDeckCardEntities(@NonNull final Game game) {
+        final DeckOfCards deckOfCards = this.deckCardCreatorService.createDeckOfCards();
+        final List<Card> cards = deckOfCards.getCards();
+        final List<GameDeckCard> lstGameDeckCard = new ArrayList<>();
+        cards.forEach(card -> {
+            final GameDeckCard gameDeckCard = createGameDeckCard(game, card);
+            lstGameDeckCard.add(gameDeckCard);
+        });
+        updateGameDeckCardOrder(lstGameDeckCard);
+        return lstGameDeckCard;
+    }
 
-	@Override
-	public void updateGameDeckCardOrder(List<GameDeckCard> lstGameDeckCard) {
-		int cardOrder = 0;
-		for (GameDeckCard gameDeckCard : lstGameDeckCard) {
-			gameDeckCard.setCardOrder(cardOrder++);
-		}
-	}
+    @Override
+    public void updateGameDeckCardOrder(@NonNull List<GameDeckCard> lstGameDeckCard) {
+        int cardOrder = 0;
+        for (GameDeckCard gameDeckCard : lstGameDeckCard) {
+            gameDeckCard.setCardOrder(cardOrder++);
+        }
+    }
 
-	GameDeckCard createGameDeckCard(final Game savedGame, Card card) {
-		final int suiteId = card.getCardSuit().getId();
-		final int gameId = savedGame.getGameId();
-		final Integer cardNumber = card.getCardNumber();
+    GameDeckCard createGameDeckCard(@NonNull final Game savedGame, Card card) {
+        final int suiteId = card.getCardSuit().getId();
+        final int gameId = savedGame.getGameId();
+        final Integer cardNumber = card.getCardNumber();
 
-		final GameDeckCardPk gameDeckCardPk = new GameDeckCardPk();
-		gameDeckCardPk.setGameId(gameId);
-		final GameDeckCard gameDeckCard = new GameDeckCard();
-		gameDeckCard.setId(gameDeckCardPk);
-		gameDeckCard.setCardSuit(suiteId);
-		gameDeckCard.setCardNumber(cardNumber);
-		return gameDeckCard;
-	}
+        final GameDeckCardPk gameDeckCardPk = new GameDeckCardPk();
+        gameDeckCardPk.setGameId(gameId);
+        final GameDeckCard gameDeckCard = new GameDeckCard();
+        gameDeckCard.setId(gameDeckCardPk);
+        gameDeckCard.setCardSuit(suiteId);
+        gameDeckCard.setCardNumber(cardNumber);
+        return gameDeckCard;
+    }
 
-	void addTheDeckGameCard(final Game game, final List<GameDeckCard> lstGameDeckCard) {
-		Assert.notNull(game, "[Assertion failed] - 'game' argument is required; it must not be null");
-		Assert.notEmpty(lstGameDeckCard,
-				"[Assertion failed] - 'lstGameDeckCard' collection must not be empty: it must contain at least 1 element");
-		final int gameId = game.getGameId();
-		updateCardsIds(lstGameDeckCard, gameId);
-		this.gameDeckCardRepo.saveAll(lstGameDeckCard);
-		final int decksNumber = game.getDecksNumber();
-		game.setDecksNumber(decksNumber + 1);
-		this.gameRepo.save(game);
-	}
+    void addTheDeckGameCard(@NonNull final Game game, @NonNull final List<GameDeckCard> lstGameDeckCard) {
+        Assert.notEmpty(lstGameDeckCard, "[Assertion failed] - 'lstGameDeckCard' collection must not be empty: it must contain at least 1 element");
+        final int gameId = game.getGameId();
+        updateCardsIds(lstGameDeckCard, gameId);
+        this.gameDeckCardRepo.saveAll(lstGameDeckCard);
+        final int decksNumber = game.getDecksNumber();
+        game.setDecksNumber(decksNumber + 1);
+        this.gameRepo.save(game);
+    }
 
-	/**
-	 * 
-	 * @param lstGameDeckCard
-	 * @param gameId
-	 * @deprecated TODO: make 'cardId' auto incremental.
-	 */
-	void updateCardsIds(final List<GameDeckCard> lstGameDeckCard, final int gameId) {
-		int maxCardId = getMaxDeckGameCardId(gameId);
-		for (GameDeckCard gameDeckCard : lstGameDeckCard) {
-			gameDeckCard.getId().setCardId(++maxCardId);
-		}
-	}
+    /**
+     * @param lstGameDeckCard
+     * @param gameId
+     * @deprecated TODO: make 'cardId' auto incremental.
+     */
+    @Deprecated(since = "1.0", forRemoval = true)
+    void updateCardsIds(@NonNull final List<GameDeckCard> lstGameDeckCard, final int gameId) {
+        int maxCardId = getMaxDeckGameCardId(gameId);
+        for (GameDeckCard gameDeckCard : lstGameDeckCard) {
+            gameDeckCard.getId().setCardId(++maxCardId);
+        }
+    }
 
-	/**
-	 * 
-	 * @param gameId
-	 * @return
-	 * @deprecated TODO: make 'cardId' auto incremental.
-	 */
-	int getMaxDeckGameCardId(final int gameId) {
-		final Optional<Integer> maxCardId = this.gameDeckCardRepo.findMaxIdCardIdByIdGameId(gameId);
+    /**
+     * @param gameId
+     * @return
+     * @deprecated TODO: make 'cardId' auto incremental.
+     */
+    @Deprecated(since = "1.0", forRemoval = true)
+    int getMaxDeckGameCardId(final int gameId) {
+        final Optional<Integer> maxCardId = this.gameDeckCardRepo.findMaxIdCardIdByIdGameId(gameId);
         return maxCardId.orElse(0);
-	}
+    }
 
-	@Override
-	public Map<CardSuit, Long> numberOfCardsPerSuitInGameDeck(String gameName) {
-		final List<Game> lstGames = this.gameRepo.findByName(gameName);
-		if (lstGames.size() > 1) {
-			throw new AppException(AppResponses.ENTITY_ALREADY_EXIST, "More than one game have the same 'gameName'!");
-		} else if (lstGames.size() == 1) {
-			final Game game = lstGames.get(0);
-			final int gameId = game.getGameId();
-			final List<GameDeckCard> findByIdGameId = this.gameDeckCardRepo.findByIdGameId(gameId);
-			final Map<Integer, Long> collect = findByIdGameId.stream()
-					.collect(Collectors.groupingBy(GameDeckCard::getCardSuit, Collectors.counting()));
-			final Map<CardSuit, Long> result = new HashMap<>();
-			collect.forEach((key, value) -> result.put(CardSuit.valueOf(key), value));
-			return result;
-		} else {
-			throw new AppException(AppResponses.ENTITY_NOT_FOUND, "There is no game with name[" + gameName + "]!");
-		}
+    @Override
+    public Map<CardSuit, Long> numberOfCardsPerSuitInGameDeck(String gameName) {
+        final List<Game> lstGames = this.gameRepo.findByName(gameName);
+        if (lstGames.size() > 1) {
+            throw new AppException(AppResponses.ENTITY_ALREADY_EXIST, SAME_GAME_NAME_MESSAGE);
+        } else if (lstGames.size() == 1) {
+            final Game game = lstGames.get(0);
+            final int gameId = game.getGameId();
+            final List<GameDeckCard> findByIdGameId = this.gameDeckCardRepo.findByIdGameId(gameId);
+            final Map<Integer, Long> collect = findByIdGameId.stream().collect(Collectors.groupingBy(GameDeckCard::getCardSuit, Collectors.counting()));
+            final Map<CardSuit, Long> result = new EnumMap<>(CardSuit.class);
+            collect.forEach((key, value) -> result.put(CardSuit.valueOf(key), value));
+            return result;
+        } else {
+            throw new AppException(AppResponses.ENTITY_NOT_FOUND, "There is no game with name[" + gameName + "]!");
+        }
 
-	}
+    }
 
-	@Override
-	public Set<CardCount> remainingCardsInGameDeck(String gameName) {
+    @Override
+    public Set<CardCount> remainingCardsInGameDeck(String gameName) {
 
-		final List<Game> lstGames = this.gameRepo.findByName(gameName);
-		if (lstGames.size() > 1) {
-			throw new AppException(AppResponses.ENTITY_ALREADY_EXIST, "More than one game have the same 'gameName'!");
-		} else if (lstGames.size() == 1) {
-			final Game game = lstGames.get(0);
-			final int gameId = game.getGameId();
-			final List<GameDeckCard> findByIdGameId = this.gameDeckCardRepo.findByIdGameId(gameId);
-			// CardSuit/CardNumber/Count
-			final Map<Integer, Map<Integer, Long>> mapCardSuitCardNumberCount = findByIdGameId.stream()
-					.collect(Collectors.groupingBy(GameDeckCard::getCardSuit,
-							Collectors.groupingBy(GameDeckCard::getCardNumber, Collectors.counting())));
+        final List<Game> lstGames = this.gameRepo.findByName(gameName);
+        if (lstGames.size() > 1) {
+            throw new AppException(AppResponses.ENTITY_ALREADY_EXIST, SAME_GAME_NAME_MESSAGE);
+        } else if (lstGames.size() == 1) {
+            final Game game = lstGames.get(0);
+            final int gameId = game.getGameId();
+            final List<GameDeckCard> findByIdGameId = this.gameDeckCardRepo.findByIdGameId(gameId);
+            // CardSuit/CardNumber/Count
+            final Map<Integer, Map<Integer, Long>> mapCardSuitCardNumberCount = findByIdGameId.stream().collect(Collectors.groupingBy(GameDeckCard::getCardSuit, Collectors.groupingBy(GameDeckCard::getCardNumber, Collectors.counting())));
 
-			final Set<CardCount> result = new TreeSet<>(new CardCountComparator());
-			mapCardSuitCardNumberCount.forEach((key, mapCardNumberCount) -> {
+            final Set<CardCount> result = new TreeSet<>(new CardCountComparator());
+            mapCardSuitCardNumberCount.forEach((key, mapCardNumberCount) -> {
                 final CardSuit cardSuit = CardSuit.valueOf(key);
                 mapCardNumberCount.forEach((cardNumber, value) -> {
-                    final CardCount cardCount = new CardCount(cardSuit, cardNumber, CardUtils.getCardType(cardNumber),
-                            value);
+                    final CardCount cardCount = new CardCount(cardSuit, cardNumber, CardUtils.getCardType(cardNumber), value);
                     result.add(cardCount);
                 });
             });
-			return result;
-		} else {
-			throw new AppException(AppResponses.ENTITY_NOT_FOUND, "There is no game with name[" + gameName + "]!");
-		}
+            return result;
+        } else {
+            throw new AppException(AppResponses.ENTITY_NOT_FOUND, "There is no game with name[" + gameName + "]!");
+        }
 
-	}
+    }
 }
